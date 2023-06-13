115c6d980a94e9a47472e0d6d3c0f61739abbe4d
Maria Komendantova <masha.komendantova@yandex.ru>
2023-06-13 17:39:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        SortedList theList = new SortedList();
+        theList.insert(20);
+        theList.insert(40);
+
+        theList.displayList();
+
+        theList.insert(10);
+        theList.insert(30);
+        theList.insert(50);
+
+        theList.displayList();
+
+        theList.remove();
+
+        theList.displayList();
+    }
+}
